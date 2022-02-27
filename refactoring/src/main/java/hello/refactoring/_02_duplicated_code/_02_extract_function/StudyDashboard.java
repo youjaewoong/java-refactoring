760beebb4f4dd727e240d13956fd91e652dd6db1
gos1004 <nnv711@gmail.com>
2022-02-27 16:56:22 +0900

    리팩토링 4. 함수 추출하기

---
+++ refactoring/src/main/java/hello/refactoring/_02_duplicated_code/_02_extract_function/StudyDashboard.java
@@ -0,0 +1,57 @@
+package hello.refactoring._02_duplicated_code._02_extract_function;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    private void printParticipants(int eventId) throws IOException {
+        // Get github issue to check homework
+        GHIssue issue = getGhIssue(eventId);
+
+        // Get participants
+        Set<String> participants = getUsernames(issue);
+
+        // Print participants
+        print(participants);
+    }
+
+	private void print(Set<String> participants) {
+		participants.forEach(System.out::println);
+	}
+
+	private Set<String> getUsernames(GHIssue issue) throws IOException {
+		Set<String> usernames = new HashSet<>();
+        issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+		return usernames;
+	}
+
+	private GHIssue getGhIssue(int eventId) throws IOException {
+		GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+		return issue;
+	}
+
+    private void printReviewers() throws IOException {
+        // Get github issue to check reviews
+    	GHIssue issue = getGhIssue(30);
+
+        // Get reviewers
+        Set<String> reviewers = getUsernames(issue);
+
+        print(reviewers);
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.printReviewers();
+        studyDashboard.printParticipants(15);
+    }
+
+}
