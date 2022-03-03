0cba40f1bab5b0062ffd3df189ac232cdc738734
gos1004 <nnv711@gmail.com>
2022-03-03 23:07:12 +0900

    리펙토링 6. 메소드 올리기

--- refactoring/src/main/java/hello/refactoring/_02_duplicated_code/_03_pull_up_method/after/Dashboard.java
+++ refactoring/src/main/java/hello/refactoring/_02_duplicated_code/_03_pull_up_method/after/Dashboard.java
@@ -20,15 +20,25 @@ public class Dashboard {
 
 	public void printUsernames(int eventId) throws IOException {
 	    // Get github issue to check homework
-	    GitHub gitHub = GitHub.connect();
-	    GHRepository repository = gitHub.getRepository("whiteship/live-study");
-	    GHIssue issue = repository.getIssue(eventId);
+	    GHIssue issue = getGhIssue(eventId);
 	
 	    // Get participants
-	    Set<String> participants = new HashSet<>();
-	    issue.getComments().forEach(c -> participants.add(c.getUserName()));
+	    Set<String> participants = getUsernames(issue);
 	
 	    // Print participants
 	    participants.forEach(System.out::println);
+	}
+
+	private Set<String> getUsernames(GHIssue issue) throws IOException {
+		Set<String> usernames = new HashSet<>();
+	    issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+		return usernames;
+	}
+
+	private GHIssue getGhIssue(int eventId) throws IOException {
+		GitHub gitHub = GitHub.connect();
+	    GHRepository repository = gitHub.getRepository("whiteship/live-study");
+	    GHIssue issue = repository.getIssue(eventId);
+		return issue;
 	}
 }
