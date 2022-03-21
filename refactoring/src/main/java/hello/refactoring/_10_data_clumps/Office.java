7773356d1b708a71c43fd85d5719ef8f3dc9e088
gos1004 <nnv711@gmail.com>
2022-03-22 00:06:26 +0900

    냄새 10. 데이터 뭉치

---
+++ refactoring/src/main/java/hello/refactoring/_10_data_clumps/Office.java
@@ -0,0 +1,34 @@
+package hello.refactoring._10_data_clumps;
+
+public class Office {
+
+    private String location;
+
+    private TelephoneNumber officePhoneNumber;
+
+
+    public Office(String location, TelephoneNumber officePhoneNumber) {
+        this.location = location;
+        this.officePhoneNumber = officePhoneNumber;
+    }
+
+    public String officePhoneNumber() {
+        return this.officePhoneNumber.toString();
+    }
+
+    public String getOfficeAreCode() {
+        return this.officePhoneNumber.getAreaCode();
+    }
+
+    public void setOfficeAreCode(String officeAreCode) {
+        this.officePhoneNumber.setAreaCode(officeAreCode);
+    }
+
+    public String getOfficeNumber() {
+        return this.officePhoneNumber.getNumber();
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officePhoneNumber .setNumber(officeNumber);
+    }
+}
