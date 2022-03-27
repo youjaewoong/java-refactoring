8d1f2c66ced9c050e125df205a79b29102fc804e
gos1004 <nnv711@gmail.com>
2022-03-27 22:12:41 +0900

    냄새 12. 반복되는 switch 문

---
+++ refactoring/src/test/java/hello/refactoring/_12_repeated_swtiches/SwitchImprovementsTest.java
@@ -0,0 +1,14 @@
+package hello.refactoring._12_repeated_swtiches;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SwitchImprovementsTest {
+
+	@Test
+	void vacationHours() {
+		SwitchImprovements si = new SwitchImprovements();
+		assertEquals(120, si.vacationHours("full-time"));
+	}
+}
