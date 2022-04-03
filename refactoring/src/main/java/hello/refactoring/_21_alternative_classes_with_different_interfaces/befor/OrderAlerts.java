e68dc1530e9bd531d60af98fc83e2a2ef57ed9f7
gos1004 <nnv711@gmail.com>
2022-04-04 00:42:13 +0900

    냄새 21. 서로 다른 인터페이스의 대안 클래스들

---
+++ refactoring/src/main/java/hello/refactoring/_21_alternative_classes_with_different_interfaces/befor/OrderAlerts.java
@@ -0,0 +1,15 @@
+package hello.refactoring._21_alternative_classes_with_different_interfaces.befor;
+
+import hello.refactoring._21_alternative_classes_with_different_interfaces.*;
+
+public class OrderAlerts {
+
+    private AlertService alertService;
+    
+    public void alertShipped(Order order) {
+        AlertMessage alertMessage = new AlertMessage();
+        alertMessage.setMessage(order.toString() + " is shipped");
+        alertMessage.setFor(order.getEmail());
+        alertService.add(alertMessage);
+    }
+}
---
+++ refactoring/src/main/java/hello/refactoring/_21_alternative_classes_with_different_interfaces/befor/OrderProcessor.java
@@ -0,0 +1,16 @@
+package hello.refactoring._21_alternative_classes_with_different_interfaces.befor;
+
+import hello.refactoring._21_alternative_classes_with_different_interfaces.*;
+
+public class OrderProcessor {
+
+    private EmailService emailService;
+
+    public void notifyShipping(Shipping shipping) {
+        EmailMessage emailMessage = new EmailMessage();
+        emailMessage.setTitle(shipping.getOrder() + " is shipped");
+        emailMessage.setTo(shipping.getEmail());
+        emailMessage.setFrom("no-reply@whiteship.com");
+        emailService.sendEmail(emailMessage);
+    }
+}
